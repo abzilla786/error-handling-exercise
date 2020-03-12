c948261188fa97a1d37baa46195860546b6202a3
abdullah ayyaz <abdullahayyaz@tcadmins-MacBook-Air.local>
2020-03-12 10:32:22 +0000

    finished exercise and solved main bug of printing empty lines at end of file

---
+++ functions.py
@@ -0,0 +1,14 @@
+# create and write order to file
+def create_write_file(file_name, order_contents):
+    with open(file_name, 'a+') as f:
+        f.write(order_contents)
+
+
+
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        y = file.readlines()
+        x = 1
+        for i in y:
+            print(str(x), '-', i.rstrip().title())
+            x += 1
---
+++ main.py
@@ -0,0 +1,49 @@
+from functions import *
+
+order_list = []
+food_list = []
+order_no = 0
+
+while True:
+    print('1. Create an order')
+    print('2. Display an order')
+    print('3. Number of orders')
+    print('4. Exit')
+    menu = int(input('Please select a menu number. e.g \'2\'\n'))
+
+    if menu == 1:
+        while True:
+            # I want a program to ask for food
+            food = str(input('What food do you want? Write \'done\' if you\'re finished\n'))
+            if 'done' in food:
+                # As a user I ask for any amount of food
+                break
+            else:
+                food_list.append(food)
+
+        # As a user I create different orders(different files)
+        [create_write_file('order' + str(order_no+1) + '.txt', i + '\n') for i in food_list]
+        order_list.append(food_list)
+        food_list = []
+        order_no += 1
+
+    if menu == 2:
+        # As a user I want to see my order
+        while True:
+            which_order = int(input('Please enter the number of the order e.g. \'1\'\n'))
+            read_file('order' + str(which_order) + '.txt')
+            input('\n Press enter/return to continue.\n')
+            break
+
+    if menu == 3:
+        print('Number of orders:', order_no)
+        input('\n Press enter/return to continue.\n')
+
+    if menu == 4:
+        print('Have a great day!')
+        break
+
+
+
+
+#TODO add exception handling
